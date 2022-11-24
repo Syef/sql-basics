04e6014cbd1d27e172649df14d191a11be0cb995
syed hafeez <hafeezsyed786.hs@gmail.com>
2022-11-24 15:43:10 +0530

    more quries added

--- basic-table.sql
+++ basic-table.sql
@@ -3,3 +3,8 @@ id int,
 name varchar(100),
 city varchar(50)
 );
+
+create table cities (
+    id int
+    name varchar(50)
+);
---
+++ insert-data.sql
@@ -0,0 +1,4 @@
+insert into students (id,name,city)values
+(1,"hafeez","guntur"),
+(2,"syed","bza"),
+(3,"sameer",h"hyd");
