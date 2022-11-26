9d24181f43e88c51f79472eeede26ee21f4c0dd0
Syed hafizullah <113756168+Syef@users.noreply.github.com>
2022-11-26 17:40:48 +0530

    Update insert-data.sql

--- insert-data.sql
+++ insert-data.sql
@@ -1,5 +1,7 @@
 insert into students (id,name,city)values
 (1,"hafeez","guntur"),
 (2,"syed","bza"),
-(3,"sameer","hyd")
-(4,"zakeer","vizak");
+(3,"sameer","hyd"),
+(4,"zakeer","vizak"),
+(5,"shoyab","bapatla"),
+(6,"rehan","chirala");
